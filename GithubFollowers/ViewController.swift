e72b31056f506ec6afc0f0737987fd22aa13d6b0
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-16 21:38:21 +0200

    Replaced Main storyboard with a programatically one

--- GithubFollowers/ViewController.swift
+++ GithubFollowers/ViewController.swift
@@ -7,6 +7,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
