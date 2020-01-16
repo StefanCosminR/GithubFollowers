9ff94fc63c894f5ae9f665f0c051dccbb5b245bd
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-16 21:12:23 +0200

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,15 @@
+// Create by Stefan Romanescu on 16/01/2020 
+// Using Swift 5.0
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
