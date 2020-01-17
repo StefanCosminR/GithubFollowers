600568e3388b83ae0bbb67c6db286432d64fc516
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-17 22:35:51 +0200

    Created setup for FollowerListVC

--- GithubFollowers/FollowerListVC.swift
+++ GithubFollowers/FollowerListVC.swift
@@ -5,7 +5,13 @@ import UIKit
 
 class FollowerListVC: UIViewController {
 
+    var username: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
