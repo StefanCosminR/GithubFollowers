a7253c9280b6c1146423a9efc333d056b6b129e0
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-17 22:10:29 +0200

    Created FollowerListVC

---
+++ GithubFollowers/FollowerListVC.swift
@@ -0,0 +1,11 @@
+// Create by Stefan Romanescu on 17/01/2020 
+// Using Swift 5.0
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
