c37338c9ec4fcc3652e8a1f293ea3dac457c4a98
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-18 17:09:59 +0200

    Extracted AlertContainer in its own view

---
+++ GithubFollowers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,31 @@
+// Create by Stefan Romanescu on 18/01/2020 
+// Using Swift 5.0
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    func configure() {
+        backgroundColor = .systemBackground
+        
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
