5c3a86fcdcf167af3f31e9bd9ab57c275f0f27ba
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-18 16:21:32 +0200

    Extended UIViewController to add method for presenting GFAlert

---
+++ GithubFollowers/UIViewController+Ext.swift
@@ -0,0 +1,17 @@
+// Create by Stefan Romanescu on 18/01/2020 
+// Using Swift 5.0
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            
+            self.present(alertVC, animated: true)
+        }
+        
+    }
+}
