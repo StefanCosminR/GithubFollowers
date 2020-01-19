714734457dee167ab096656ead86a8baefa12a0b
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-19 13:19:25 +0200

    Tried optimizing compiling of regex to happen only once

--- GithubFollowers/Extensions/String+Ext.swift
+++ GithubFollowers/Extensions/String+Ext.swift
@@ -3,10 +3,12 @@
 
 import Foundation
 
+fileprivate let githubUsernameRegex = try! NSRegularExpression(pattern: #"^[a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38}$"#)
+
 extension String {
     var isValidGithubUsername: Bool {
-        let regexPattern = #"^[a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38}$"#
-        return range(of: regexPattern, options: .regularExpression) != nil
+        let range = NSRange(location: 0, length: utf16.count)
+        return githubUsernameRegex.firstMatch(in: self, options: [], range: range) != nil
     }
     
 }
