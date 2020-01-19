8ac64e507ae9da7bb695f275655442aa837cb20a
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-19 08:22:09 +0200

    Created GFAvatarImageView

---
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,16 @@
+// Create by Stefan Romanescu on 19/01/2020 
+// Using Swift 5.0
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
