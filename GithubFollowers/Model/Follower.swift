70d17302e802ada6cf65d09ed0c3776202e09f5c
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-19 09:28:48 +0200

    Implemented DiffableDataSource  for FollowerListVC

--- GithubFollowers/Model/Follower.swift
+++ GithubFollowers/Model/Follower.swift
@@ -1,7 +1,7 @@
 // Create by Stefan Romanescu on 18/01/2020 
 // Using Swift 5.0
 
-struct Follower: Codable {
+struct Follower: Codable, Hashable {
     let avatarUrl: String
     let login: String
 }
