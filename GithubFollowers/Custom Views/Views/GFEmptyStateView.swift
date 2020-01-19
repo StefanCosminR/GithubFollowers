0d0a64e4f11aab14bc84d2a715647aa7bc73b0cf
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-19 20:52:44 +0200

    Created EmptyStateView

---
+++ GithubFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,57 @@
+// Create by Stefan Romanescu on 19/01/2020 
+// Using Swift 5.0
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+    let messageLable = GFTitleLable(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(message: String) {
+        self.init(frame: .zero)
+        messageLable.text = message
+    }
+    
+    private func configure() {
+        addSubview(messageLable)
+        addSubview(logoImageView)
+        configureMessageLable()
+        configureLogoImageView()
+    }
+    
+    private func configureMessageLable() {
+        messageLable.numberOfLines = 3
+        messageLable.textColor = .secondaryLabel
+        messageLable.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLable.topAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLable.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLable.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLable.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureLogoImageView() {
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        ])
+    }
+    
+}
