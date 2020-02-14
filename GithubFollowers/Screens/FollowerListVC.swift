47b4ee46866d41f01e5ee3178bca6989cde274a1
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-02-14 20:02:31 +0200

    Added navigation to UserInfoVC

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -14,6 +14,7 @@ class FollowerListVC: UIViewController {
     var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -129,15 +130,29 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destinationVC = UserInfoVC()
+        destinationVC.username = follower.login
+        
+        let navController = UINavigationController(rootViewController: destinationVC)
+        present(navController, animated: true)
+        
+    }
 }
 
 extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text else { return }
+        isSearching = true
         
         if filter.isEmpty {
             filteredFollowers.removeAll()
+            isSearching = false
             updateData(on: followers)
         } else {
             filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
@@ -147,6 +162,7 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         filteredFollowers.removeAll()
+        isSearching = false
         updateData(on: followers)
     }
     
---
+++ GithubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfoVC.swift
+//  GithubFollowers
+//
+//  Created by Stefan Romanescu on 14/02/2020.
+//  Copyright © 2020 StefanCosmin. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
