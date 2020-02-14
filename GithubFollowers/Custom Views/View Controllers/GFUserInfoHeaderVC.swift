5fb03cff87a007c1e8aa4a292424389ddab97b5b
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-02-14 20:23:54 +0200

    Added custom views

---
+++ GithubFollowers/Custom Views/View Controllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,18 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GithubFollowers
+//
+//  Created by Stefan Romanescu on 14/02/2020.
+//  Copyright © 2020 StefanCosmin. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
